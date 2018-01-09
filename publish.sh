13c1f9150d2c2f3b7fbd6026787a77db0237669d
Evgeny Demchenko <little_pea@list.ru>
2018-01-09 16:47:29 +0800

    update publish script

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ jupyter-nbconvert --to slides talk.ipynb
 mv talk.slides.html index.html
 git add index.html
 git commit -a -m "update pages"
-git pull origin master gh-pages && git push origin master gh-pages
+git pull origin gh-pages && git push origin gh-pages
 
 git checkout master
 rm -rf index.html
